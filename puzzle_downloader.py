75c15367e3f22a412c6d7c0346f724373ebb679f
peterherro <45834855+peterherro@users.noreply.github.com>
2019-12-17 14:14:57 +1100

    added base files

---
+++ puzzle_downloader.py
@@ -0,0 +1,85 @@
+import requests
+from bs4 import BeautifulSoup as bs
+
+import json
+import os
+import itertools
+import random
+
+from tqdm import tqdm
+from copy import deepcopy
+from collections import defaultdict
+
+def chunks(l,size):
+	for i in range(0,len(l),size):
+		yield l[i:i+size]
+
+class WBDownloader:
+
+	def __init__(self,difficulty='easy'):
+
+		self.base_url = 'https://wordbrain.info'
+		self.headers = {
+		'Accept':'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9',
+		'User-Agent':'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/79.0.3945.79 Safari/537.36'
+		}
+		self.difficulty = difficulty
+
+		home_resp = requests.get(self.base_url + '/en/',headers=self.headers)
+		if not home_resp.status_code == 200:
+			raise Exception('Cannot connect to WordBrain home page.')
+			return
+		home_soup = bs(home_resp.content,'html.parser')
+		self.endpoints = [pack.a['href'] for pack in home_soup.select('div.pack')]
+
+		self.difficulties = {
+		'easy':self.endpoints[:self.endpoints.index('/en/turtle/')+1],
+		'medium':self.endpoints[self.endpoints.index('/en/turtle/')+1:self.endpoints.index('/en/lion/')+1],
+		'hard':self.endpoints[self.endpoints.index('/en/lion/')+1:self.endpoints.index('/en/minotaur/')+1],
+		'impossible':self.endpoints[self.endpoints.index('/en/minotaur/')+1:]
+		}
+
+		if not self.difficulty in self.difficulties.keys():
+			raise Exception(f'{self.difficulty} is not supported.')
+			return
+
+	def download_one_puzzle(self):
+		try:
+			endpoint = random.choice(self.difficulties[self.difficulty])
+			puzzle_resp = requests.get(self.base_url + endpoint,headers=self.headers)
+			if not puzzle_resp.status_code == 200:
+				raise Exception('Cannot connect to WordBrain home page.')
+				return
+			puzzle_soup = bs(puzzle_resp.content,'html.parser')
+
+			all_puzzles = [p for p in puzzle_soup.select('li a') if p.span]
+			selected_puzzle = random.choice(all_puzzles)
+
+			solution = selected_puzzle.select('span.solution')[0].text.split(',')
+			puzzle = selected_puzzle.select('span.letterblock')[0]
+
+			return {'puzzle':[[cell.text.upper() for cell in line] for line in \
+			list(chunks(puzzle.find_all('span'),len(puzzle.find_all('br'))))],'solution':solution}
+		except Exception as e:
+			return e
+
+	def download_all_puzzles(self,from_endpoint=False):
+		for difficulty in tqdm(self.difficulties):
+			puzzle_data = []
+			for endpoint in self.difficulties[difficulty]:
+				puzzle_resp = requests.get(self.base_url + endpoint,headers=self.headers)
+				if not puzzle_resp.status_code == 200:
+					raise Exception('Cannot connect to WordBrain home page.')
+					return
+				puzzle_soup = bs(puzzle_resp.content,'html.parser')
+
+				for selected_puzzle in [p for p in puzzle_soup.select('li a') if p.span]:
+					solution = selected_puzzle.select('span.solution')[0].text.split(',')
+					puzzle = selected_puzzle.select('span.letterblock')[0]
+					puzzle_data.append({'puzzle':[[cell.text.upper() for cell in line] for line in \
+					list(chunks(puzzle.find_all('span'),len(puzzle.find_all('br'))))],'solution':solution})
+			if from_endpoint:
+				
+			else:
+				with open(difficulty+'.json','w') as download_file:
+					json.dump(puzzle_data,download_file)
---
+++ word_brain.py
@@ -0,0 +1,142 @@
+import requests
+import json
+import os
+import sys
+import itertools
+
+from copy import deepcopy
+from collections import defaultdict
+from puzzle_downloader import *
+
+class WordBrain:
+
+	def __init__(self,
+		puzzle_fetch=False,
+		puzzle_fetch_difficulty='easy'
+		):
+
+		if not 'dictionary.json' in os.listdir():
+			resp = requests.get('https://raw.githubusercontent.com/dwyl/english-words/master/words_alpha.txt')
+			self.dictionary = resp.content.decode('utf-8').split('\r\n')
+			with open('dictionary.json','w') as d:
+				json.dump(self.dictionary,d)
+		else:
+			with open('dictionary.json') as d:
+				self.dictionary = json.load(d)
+
+		if not puzzle_fetch:
+			try:
+				puzzle_file = [e for e in os.listdir() if puzzle_fetch_difficulty in e][0]
+				with open(puzzle_file) as pf:
+					self.puzzles = json.load(pf)
+					self.puzzle_located = random.choice(self.puzzles)
+
+					self.solution = self.puzzle_located['solution']
+					self.puzzle = self.puzzle_located['puzzle']
+					self.word_lengths = [len(w) for w in self.solution]
+			except Exception as e:
+				return e
+				sys.exit()
+		else:
+			try:
+				self.downloader = WBDownloader(difficulty=puzzle_fetch_difficulty)
+			except Exception as e:
+				return e
+				sys.exit()
+			self.puzzle_downloaded = self.downloader.download_one_puzzle()
+
+			self.solution = self.puzzle_downloaded['solution']
+			self.puzzle = self.puzzle_downloaded['puzzle']
+			self.word_lengths = [len(w) for w in self.solution]
+
+		if self.puzzle:
+			self.grid_coords = [[(x,y) for y in range(len(i))] for x,i in enumerate(self.puzzle)]
+			self.total_avalable_coords = list(itertools.chain.from_iterable(self.grid_coords))
+			self.words = defaultdict(list)
+			self.total_possible_words = []
+
+	def input_own_puzzle(self,puzzle,word_lengths):
+		self.puzzle = [[e.upper() for e in line] for line in puzzle]
+		self.solution = None
+		self.word_lengths = word_lengths
+
+	def search_for_word(self,
+		loc=(0,0),
+		visited=[(0,0)],
+		words=[],
+		word_lengths=None,
+		curr_word_length=None,
+		graph=None):
+
+		if len(visited) == curr_word_length \
+		and ''.join([graph[c[0]][c[1]] for c in visited]).lower() in self.dictionary:
+			if not word_lengths:
+				self.total_possible_words.append(words + [''.join([graph[c[0]][c[1]] for c in visited])])
+				return
+			new_graph = self.rearrange_graph(visited,graph)
+			# print(new_graph)
+			cwl = word_lengths[0]
+			if len(word_lengths) == 1:
+				wl = []
+			else:
+				wl = word_lengths[1:]
+			words = words + [''.join([graph[c[0]][c[1]] for c in visited])]
+			for i,line in enumerate(new_graph):
+				for j,l in enumerate(line):
+					if l:
+						self.search_for_word(
+							loc=(i,j),
+							visited=[(i,j)],
+							words=words,
+							word_lengths=wl,
+							curr_word_length=cwl,
+							graph=new_graph
+							)
+		else:
+			available_coords = self.get_possible_moves(loc,visited,graph)
+			for coord in available_coords:
+				self.search_for_word(
+				loc=coord,
+				visited=visited+[coord],
+				words=words,
+				word_lengths=word_lengths,
+				curr_word_length=curr_word_length,
+				graph=graph
+				)
+
+	def rearrange_graph(self,word,graph):
+		graph = deepcopy(graph)
+		for c in word:
+			graph[c[0]][c[1]] = ''
+		for i,line in enumerate(graph[:-1]):
+			for j,letter in enumerate(line):
+				if letter and graph[i+1][j] == '':
+					graph[i+1][j] = letter
+					graph[i][j] = ''
+		return graph
+
+	def solve_puzzle(self,word_lengths=None):
+		if not word_lengths:
+			word_lengths = self.word_lengths
+		self.total_possible_words = []
+		for coord in self.total_avalable_coords:
+			cwl = word_lengths[0]
+			if len(word_lengths) == 1:
+				wl = []
+			else:
+				wl = word_lengths[1:]
+			word = self.search_for_word(
+				loc=coord,
+				visited=[coord],
+				word_lengths=wl,
+				curr_word_length=cwl,
+				graph=self.puzzle
+				)
+		return self.total_possible_words
+
+	def get_possible_moves(self,position,visited,graph):
+		x,y = position
+		return [pos for pos in [(x-1,y),(x-1,y+1),(x,y+1),(x+1,y+1),
+		(x+1,y),(x+1,y-1),(x,y-1),(x-1,y-1)]
+		if pos in self.total_avalable_coords and pos not in visited
+		and graph[pos[0]][pos[1]] != '']
